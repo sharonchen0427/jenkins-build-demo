ac8825fc73239519cdf6a583e3b35556418fbb6b
-chenshumei <-chensharon0427@gmail.com>
2021-08-01 11:01:37 -0400

    Initial commit

---
+++ src/main/java/com/example/jenkinsbuilddemo/JenkinsBuildDemoApplication.java
@@ -0,0 +1,13 @@
+//package com.example.jenkinsbuilddemo;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class JenkinsBuildDemoApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(JenkinsBuildDemoApplication.class, args);
+//    }
+//
+//}
